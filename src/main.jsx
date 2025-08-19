343b40c74413369d7bb8ed2e0737b5ba70803b94
Waleed Imran <hafizwaleedimran9827159@gamil.com>
2025-08-19 18:21:57 +0500

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,20 @@
-
-
+/* eslint-disable no-undef */
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 
 import App from "./App.jsx";
 import { RouterProvider } from "react-router-dom";
 import { Routing } from "./router/Rounting.jsx";
 import { Provider } from "react-redux";
 import store from "./redux/store.jsx";
+
+import { inject } from "@vercel/analytics";
+
+if (process.env.NODE_ENV === "production") {
+  console.log = () => {};
+}
+
+inject({ debug: false });
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
